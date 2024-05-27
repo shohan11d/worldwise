ef7f2e0a39b5d76b02c546873dc31eb6cac3a8db
shohan11d <shohan11d@gmail.com>
2024-05-27 11:49:16 +0600

    css module added

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "./AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/pages/AppNav.jsx
@@ -0,0 +1,6 @@
+import styles from "./AppNav.module.css";
+function AppNav() {
+  return <nav className={styles.nav}>app navigation</nav>;
+}
+
+export default AppNav;
