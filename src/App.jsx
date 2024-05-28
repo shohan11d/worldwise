205a2842d7ab69e9443fb1c88b38c2dcba8362ad
shohan11d <shohan11d@gmail.com>
2024-05-29 05:05:26 +0600

    nested routes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login"; 
+import Login from "./pages/Login";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />}></Route>
+          <Route index element={<Homepage />}></Route>
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List</p>}></Route>
+            <Route path="cities" element={<p>List of cities</p>}></Route>
+            <Route path="countries" element={<p>Countries</p>}></Route>
+            <Route path="form" element={<p>form</p>}></Route>
+          </Route>
+
           <Route path="/login" element={<Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
