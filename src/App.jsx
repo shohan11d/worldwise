a5d830f4ffc442e0e7511a1b611d58ea3854212b
shohan11d <shohan11d@gmail.com>
2024-05-29 08:36:02 +0600

    city list implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,28 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./ components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading dat...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <div>
       <BrowserRouter>
@@ -13,14 +34,20 @@ function App() {
           <Route index element={<Homepage />}></Route>
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>List</p>}></Route>
-            <Route path="cities" element={<p>List of cities</p>}></Route>
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            ></Route>
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            ></Route>
             <Route path="countries" element={<p>Countries</p>}></Route>
             <Route path="form" element={<p>form</p>}></Route>
           </Route>
 
-          <Route path="/login" element={<Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
