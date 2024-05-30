87d02a96adb5e1a6176d808daeb2a716763eb5d4
shohan11d <shohan11d@gmail.com>
2024-05-31 00:16:26 +0600

    programmitc navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Login from "./pages/Login";
 import CityList from "./ components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./ components/CountryList";
-import City from './ components/City'
+import City from "./ components/City";
+import Form from "./ components/Form";
 const BASE_URL = "http://localhost:8000";
 function App() {
   const [cities, setCities] = useState([]);
@@ -51,7 +52,7 @@ function App() {
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             ></Route>
-            <Route path="form" element={<p>form</p>}></Route>
+            <Route path="form" element={<Form />}></Route>
           </Route>
 
           <Route path="*" element={<PageNotFound />}></Route>
