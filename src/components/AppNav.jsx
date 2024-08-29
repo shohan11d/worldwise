5a11c7b5080f1595180ac87f2b8b17e202e9bfdb
shohan11d <shohan11d@gmail.com>
2024-08-29 14:17:00 +0600

    nested route added

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,6 +1,16 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Appnav.module.css';
 function AppNav() {
-  return <nav className={styles.nav}> App Navigaion</nav>;
+  return <nav className={styles.nav}>
+    <ul>
+      <li>
+        <NavLink to='cities'>Cities</NavLink>
+      </li>
+      <li>
+        <NavLink to='countries'>Countries</NavLink>
+      </li>
+    </ul>
+  </nav>;
 }
 
 export default AppNav;
