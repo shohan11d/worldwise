f127b45a1e0ed70dac7c48ea0048d510c65f241e
shohan11d <shohan11d@gmail.com>
2024-08-30 11:14:29 +0600

    dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
-import index from '../index.css';
-import { useEffect, useState } from 'react';
 import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
 const BASE_URL = 'http://localhost:3000';
+
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -40,7 +43,11 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="cities/:id" element={<City />}></Route>
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
